7fbfd84cba2289cf31847ee9fcdc62f803ffbeac
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-03-26 15:58:32 -0400

    Build goalCell, Configure GoalCell to receive data.

---
+++ Goal-Post-App/View/GoalCell.swift
@@ -0,0 +1,28 @@
+//
+//  GoalCell.swift
+//  Goal-Post-App
+//
+//  Created by Johnny Perdomo on 3/26/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    //IBOutlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) { //when we init these cells the data will input into the cells
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount) //converts int into string, the textual value, not the numerical value
+        
+        
+        
+    }
+    
+}
