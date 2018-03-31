802ba3e70224ea113fb46bffdc1f6334a62c614c
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-03-30 22:58:08 -0400

    fix txtField issue/ create extensions to bind objects to keyboard and add goalType selected colors

--- Goal-Post-App/Controller/CreateGoalVC.swift
+++ Goal-Post-App/Controller/CreateGoalVC.swift
@@ -12,26 +12,33 @@ class CreateGoalVC: UIViewController {
 
     
     //IBOutlets
-    @IBOutlet weak var goalTxtView: UITextView!
+    @IBOutlet weak var goalTxtView: UITextField!
     @IBOutlet weak var shortTermBtn: UIButton!
     @IBOutlet weak var LongTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType: GoalType = .shortTerm //setting it equal to short term from the beginning
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextBtn.bindToKeyboard() //if you call this, when keyboard goes up, nxt button goes up with it as well
+        shortTermBtn.setSelectedColor() //sets the button to be dark green
+        LongTermBtn.setDeselctedColor() //sets btn to be light green
     }
     
     //IBActions
  
-    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
-        
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) { //switch btn colors if this one is pressed
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        LongTermBtn.setDeselctedColor()
     }
     
-    @IBAction func longTermBtnWasPressed(_ sender: Any) {
-        
+    @IBAction func longTermBtnWasPressed(_ sender: Any) { //switch btn colors if this one is pressed
+        goalType = .longTerm
+        shortTermBtn.setDeselctedColor()
+        LongTermBtn.setSelectedColor()
     }
     
     
